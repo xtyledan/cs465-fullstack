d5eef882496408cd692fe4c2e8088a03a8ca4de0
Tyler Daniels <tdaniels3526@gmail.com>
2025-07-18 19:37:19 -0700

    Module 3 completed baseline

--- Desktop/travlr/app.js
+++ Desktop/travlr/app.js
@@ -10,7 +10,7 @@ var travelRouter = require('./app_server/routes/travel');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 const hbs = require('hbs');
 app.set('view engine', 'hbs');
--- Desktop/travlr/app_server/controllers/travel.js
+++ Desktop/travlr/app_server/controllers/travel.js
@@ -1,6 +1,12 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
-const travel = (reg, res) => {
-    res.render('travel', { title: 'travlr Getaways'});
+const travel = (req, res) => {
+    res.render('travel', {
+        title: 'travlr Getaways',
+        trips: trips.trips  // Pass trips data to the view
+    });
 };
 
 module.exports = {
